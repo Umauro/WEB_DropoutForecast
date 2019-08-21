32017d6dea237af15c58af9445627825294ea88c
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2019-08-20 22:57:24 -0400

    add angularjs app structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WEB_DropoutForecast
+
+Para ejecutar de manera fácil es necesario correr el siguiente comando (con python 3.x)
+~~~ 
+python3 -m http.server
+~~~
+
+Después acceder a la url '''localhost:8000'''
---
+++ js/app.js
@@ -0,0 +1,33 @@
+var web_dropoutforecast = angular.module('web_dropoutforecast',["ngRoute"]);
+
+//Component Configuration
+web_dropoutforecast.component('navbarPage',{
+    templateUrl: "views/navbar.html"
+})
+.component('footerPage',{
+    templateUrl: "views/footer.html"
+});
+
+//App Routing
+web_dropoutforecast.config(function($routeProvider, $locationProvider){
+    $routeProvider
+    .when("/",{
+        templateUrl: "views/home.html"
+    })
+    .when("/equipo",{
+        templateUrl: "views/equipo.html"
+    })
+    .when("/proyecto",{
+        templateUrl: "views/proyecto.html"
+    })
+    .when("/contacto",{
+        templateUrl: "views/contacto.html"
+    })
+    .when("/redes",{
+        templateUrl: "views/redesociales.html"
+    })
+    .otherwise({
+        templateUrl: "views/home.html"
+    });
+    $locationProvider.hashPrefix("");
+})
