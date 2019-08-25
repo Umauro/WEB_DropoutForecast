123ddf6f1d9db95541a27fdbe289b4b292285b56
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2019-08-24 22:56:20 -0400

    change colors, set same font in all views and change content

--- js/equipoCtrl.js
+++ js/equipoCtrl.js
@@ -2,57 +2,58 @@ angular.module('web_dropoutforecast').controller('equipoCtrl',['$scope',function
     $scope.team_members = [
         {
             name:'Felipe Vásquez',
-            role:'Jefe Proyecto - Backend Developer',
+            role:'Jefe Proyecto',
+            subrole:'Backend Developer',
             description:'niño backend',
             linkedin:'https://www.linkedin.com/in/felipe-v%C3%A1squez-5a9267134/',
             github:'https://github.com/DiFroggy',
             twitter:'https://twitter.com/jawas_ownz',
-            img:'difroggy.jpg'
+            img:'difroggy.jpg',
+            instagram:'https://www.instagram.com/difroggy/'
         },
         {
             name:'Ignacio Salas',
-            role:'Jefe Técnico - Full Stack Developer',
+            role:'Jefe Técnico',
+            subrole:'Full Stack Developer',
             description:'niño fullstack',
             linkedin:'https://www.linkedin.com/in/isalas012/',
             github:'https://github.com/isalas012',
             twitter:'https://twitter.com/PipexxGfx',
-            img:'nacho.jpg'
+            img:'nacho.jpg',
+            instagram:"https://www.instagram.com/soundless._/"
         },
         {
             name:'Andrés Huerta',
-            role:'Scrum Master - Backend Developer',
+            role:'Scrum Master',
+            subrole:'Backend Developer',
             description:'niño backend',
             linkedin:'https://www.linkedin.com/in/andr%C3%A9s-huerta-rojo-248551181/',
             github:'https://github.com/AHRojo',
             twitter:'https://twitter.com/ahrojo1',
-            img:'andres2.jpg'
+            img:'andres2.jpg',
+            instagram:'https://www.instagram.com/andres_ihr/'
         },
         {
             name:'Felipe Vega',
-            role:'Machine Learning Engineer - Backend Developer',
+            role:'Machine Learning Engineer',
+            subrole:'Backend Developer',
             description:'niño modelo',
             linkedin:'https://www.linkedin.com/in/felipe-vega-valencia-1b2a33181/',
             github:'https://github.com/Umauro',
             twitter:'https://twitter.com/PipexxGfx',
-            img:'mauro.jpg'
-        },
-        {
-            name:'Felipe Vega',
-            role:'Machine Learning Engineer - Backend Developer',
-            description:'niño modelo',
-            linkedin:'https://www.linkedin.com/in/felipe-vega-valencia-1b2a33181/',
-            github:'https://github.com/Umauro',
-            twitter:'https://twitter.com/PipexxGfx',
-            img:'mauro.jpg'
+            img:'mauro.jpg',
+            instagram:'https://www.instagram.com/pipexxgfx/'
         },
         {
             name:'Victor Zavala',
-            role:'Jefe Marketing - Frontend Developer',
+            role:'Jefe Marketing',
+            subrole:'Frontend Developer',
             description:'niño frontend',
             linkedin:'https://www.linkedin.com/in/victor-zavala-08ab0110b/',
             github:'https://github.com/VictorZ7',
             twitter:'https://twitter.com/PipexxGfx',
-            img:'victor.jpg'
+            img:'victor.jpg',
+            instagram:'https://www.instagram.com/victorzavala_a/'
         }
     ];
 }]);
